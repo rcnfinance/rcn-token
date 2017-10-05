87f68d813e35e38439b50424b87b0f558d4c7a49
Agustin Aguilar <agustin@bitpagos.co>
2017-10-05 11:47:57 -0300

    Allow finalize crowdsale if pending amount is less than min

--- contracts/RCNToken.sol
+++ contracts/RCNToken.sol
@@ -78,7 +78,7 @@ contract RCNToken is StandardToken, Crowdsale {
       if (isFinalized) throw;
       if (msg.sender != ethFundDeposit) throw; // locks finalize to the ultimate ETH owner
       if(totalSupply < tokenCreationMin) throw;      // have to sell minimum to move to operational
-      if(block.number <= fundingEndBlock && totalSupply != tokenCreationCap) throw;
+      if(block.number <= fundingEndBlock && totalSupply < tokenCreationCap - minBuyTokens) throw;
       // move to operational
       isFinalized = true;
       if(!ethFundDeposit.send(this.balance)) throw;  // send the eth to Ripio International
