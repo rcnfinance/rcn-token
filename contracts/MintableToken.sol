8a387e6610542eaed80c951c52b31c7d46cb0641
Agustin Aguilar <agusxrun@gmail.com>
2017-10-05 22:36:28 -0300

    canMint MintableToken

--- contracts/MintableToken.sol
+++ contracts/MintableToken.sol
@@ -13,9 +13,16 @@ import './zeppelin/SafeMath.sol';
 
 contract MintableToken is StandardToken, Ownable {
   using SafeMath for uint256;
-
   event Mint(address indexed to, uint256 amount);
   event MintFinished();
+
+  bool public mintingFinished = false;
+
+
+  modifier canMint() {
+    require(!mintingFinished);
+    _;
+  }
 
   /**
    * @dev Function to mint tokens
@@ -23,11 +30,21 @@ contract MintableToken is StandardToken, Ownable {
    * @param _amount The amount of tokens to mint.
    * @return A boolean that indicates if the operation was successful.
    */
-  function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
+  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
     totalSupply = totalSupply.add(_amount);
     balances[_to] = balances[_to].add(_amount);
     Mint(_to, _amount);
     Transfer(0x0, _to, _amount);
+    return true;
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting() onlyOwner public returns (bool) {
+    mintingFinished = true;
+    MintFinished();
     return true;
   }
 }
