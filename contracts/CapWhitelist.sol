a6767bf6caae5b3d655bb9425bbdebf23bb0647a
Agustin Aguilar <agustin@bitpagos.co>
2017-10-09 11:47:01 -0300

    New whitelist crowdsale

--- contracts/CapWhitelist.sol
+++ contracts/CapWhitelist.sol
@@ -2,16 +2,28 @@ pragma solidity ^0.4.11;
 
 contract CapWhitelist {
     address public owner;
-    mapping (address => bool) public whitelist;
+    mapping (address => uint256) public whitelist;
+
+    event Set(address _address, uint256 _amount);
 
     function CapWhitelist() {
         owner = msg.sender;
         // Replace in prod
-        whitelist[address(0xeab987dc90b29b9c0ec4863463697907e7ce1d55)] = true;
+        setWhitelistInternal(address(0xeab987dc90b29b9c0ec4863463697907e7ce1d55), 100000000000000000000000000000000000000000);
     }
 
     function destruct() {
         require(msg.sender == owner);
         selfdestruct(owner);
+    }
+
+    function setWhitelisted(address _address, uint256 _amount) {
+        require(msg.sender == owner);
+        setWhitelistInternal(_address, _amount);
+    }
+
+    function setWhitelistInternal(address _address, uint256 _amount) private {
+        whitelist[_address] = _amount;
+        Set(_address, _amount);
     }
 }
